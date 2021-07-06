4e83b06624eb086e25edd98630d30e2575edb2d5
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2021-07-06 14:48:28 +0400

    Random players

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,5 +1,87 @@
 public class Exercise {
     public static void main(String[] args) {
+        Example ex = new Example();
+        ex.setLogin("Yunus");
+        ex.setPassword("12345");
+
+        Example ex2 = new Example();
+        ex2.setLogin("Ayxan");
+        ex2.setPassword("22222");
+
+        Animal animal = new Tiger();
+        Animal animal1 = new Lion();
+    }
+}
+
+class Example{
+    private String login = "Suleyman";
+    private String password = "12345";
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void getData(){
+        System.out.println(login + " " + password);
+    }
+}
+
+class ExampleNew extends Example implements ExampleOld, ExampleInterface{
+    private String number;
+
+    public void getData(){
+        System.out.println(number);
+    }
+
+    @Override
+    public void getInfo() {
 
     }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void getTime() {
+
+    }
+}
+
+interface ExampleOld{
+    void getInfo();
+    String getName();
+}
+
+interface ExampleInterface{
+    void getTime();
+}
+
+class Animal{
+
+}
+
+class Tiger extends Animal{
+
+}
+
+class Lion extends Animal{
+
+}
+
+class Elephant extends Animal{
+
 }
---
+++ src/RandomPlayer.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomPlayer {
+    public static void main(String[] args){
+        List<String> names = new ArrayList<>();
+        names.add("Ali Qasimov");
+        names.add("Ali Ismayilov");
+        names.add("Altay Abbasquliyev");
+        names.add("Nihad");
+        names.add("Magomed");
+        names.add("Shahmar");
+        names.add("Huseyn");
+        names.add("Deniz");
+
+        Random random = new Random();
+        for(int i=0;i<4;i++){
+            String first = names.get(random.nextInt(names.size()));
+            names.remove(first);
+            String second = names.get(random.nextInt(names.size()));
+            names.remove(second);
+            System.out.println(first + " and " + second);
+
+
+        }
+
+    }
+}
