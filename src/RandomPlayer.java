994b11fe8ee771994b64ba5e5f6bdc0b3894c0b2
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2021-07-13 16:22:19 +0400

    Update random players and cereated simple way to calculate factorial of num via recursion.

--- src/RandomPlayer.java
+++ src/RandomPlayer.java
@@ -3,42 +3,18 @@ import java.util.List;
 import java.util.Random;
 
 public class RandomPlayer {
-//    public static void main(String[] args){
-//        List<String> names = new ArrayList<>();
-//        names.add("Ali Qasimov");
-//        names.add("Ali Ismayilov");
-//        names.add("Altay Abbasquliyev");
-//        names.add("Nihad");
-//        names.add("Magomed");
-//        names.add("Shahmar");
-//        names.add("Huseyn");
-//        names.add("Deniz");
-//
-//        Random random = new Random();
-//        for(int i=0;i<4;i++){
-//            String first = names.get(random.nextInt(names.size()));
-//            names.remove(first);
-//            String second = names.get(random.nextInt(names.size()));
-//            names.remove(second);
-//            System.out.println(first + " and " + second);
-//
-//
-//        }
-//
-//    }
-public static void main(String[] args){
-        Random random = new Random();
+    public static void main(String[] args){
         List<String> names = new ArrayList<>();
-        names.add("Elnur");
-        names.add("Said");
-        names.add("Kazim");
-        names.add("Ayxan");
-        names.add("Alishan");
+        names.add("Ali Qasimov");
+        names.add("Ali Ismayilov");
+        names.add("Altay Abbasquliyev");
+        names.add("Nihad");
+        names.add("Magomed");
+        names.add("Shahmar");
         names.add("Huseyn");
-        names.add("Ramazan");
-        names.add("" + (random.nextInt(3)+1));
+        names.add("Deniz");
 
-
+        Random random = new Random();
         for(int i=0;i<4;i++){
             String first = names.get(random.nextInt(names.size()));
             names.remove(first);
@@ -50,4 +26,28 @@ public static void main(String[] args){
         }
 
     }
+//public static void main(String[] args){
+//        Random random = new Random();
+//        List<String> names = new ArrayList<>();
+//        names.add("Elnur");
+//        names.add("Said");
+//        names.add("Kazim");
+//        names.add("Ayxan");
+//        names.add("Alishan");
+//        names.add("Huseyn");
+//        names.add("Ramazan");
+//        names.add("" + (random.nextInt(3)+1));
+//
+//
+//        for(int i=0;i<4;i++){
+//            String first = names.get(random.nextInt(names.size()));
+//            names.remove(first);
+//            String second = names.get(random.nextInt(names.size()));
+//            names.remove(second);
+//            System.out.println(first + " and " + second);
+//
+//
+//        }
+//
+//    }
 }
